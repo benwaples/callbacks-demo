1ec2fc5db3c9d64594e4d635654b1920e52c4e8d
benwaples <benwaples@gmail.com>
2021-03-23 16:13:38 -0700

    extra reduce test for intial value param

--- reduce/reduce.test.js
+++ reduce/reduce.test.js
@@ -10,4 +10,14 @@ describe('reduce array method', () => {
 
     expect(actual).toEqual(21)
   })
+
+  it('should return a new array based evaluation in an array without initial value', () => {
+    const numbers = [1, 2, 3, 4, 5, 6]
+
+    const reducer = (acc, curr) => acc + curr
+
+    const actual = reduce(numbers, reducer)
+    
+    expect(actual).toEqual(22)
+  })
 })
